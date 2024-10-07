7e1e8909a26ec63ad1a9f3a3dd8b7c5c90c477b7
negashree <negashree2203@gmail.com>
2024-10-07 18:26:05 +0530

    Updated the dropdown menu component

--- codsoft.js
+++ codsoft.js
@@ -9,6 +9,7 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname) {
+    
     for (let tablink of tablinks) {
         tablink.classList.remove("active-link");
     }
@@ -17,6 +18,34 @@ function opentab(tabname) {
         tabcontent.classList.remove("active-tab");
     }
 
-    document.querySelector(`[onclick="opentab('${tabname}')"]`).classList.add("active-link");
+    event.currentTarget.classList.add("active-link");
+
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+
+
+const sections = document.querySelectorAll('section'); 
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const targetSection = document.querySelector(link.getAttribute('href'));
+
+        sections.forEach(section => {
+            section.classList.remove('active');
+        });
+
+        if (targetSection) {
+            targetSection.classList.add('active');
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const percentCircles = document.querySelectorAll('.percent svg circle:nth-child(2)');
+    
+    percentCircles.forEach(circle => {
+        circle.classList.add('active');
+    });
+});   
